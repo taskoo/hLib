f6deb4807aa9f9a22de98b0dddb106b6d82576b3
Lenovo <Lenovo@LENOVO-PC>
2017-03-28 21:44:13 +0200

    Add project files.

---
+++ hLib/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hLib.Startup))]
+namespace hLib
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
